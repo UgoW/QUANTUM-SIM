69a0e0e6c338bd52248ef24ed8af0fb9ac41836d
Williampasset <william.passet@student.junia.com>
2026-01-15 14:13:04 +0100

    feat(type-validation): add type on method and function

--- src/quantum_sim/errors/exceptions.py
+++ src/quantum_sim/errors/exceptions.py
@@ -7,20 +7,20 @@ class QuantumSimError(Exception):
 
 class InvalidParameterError(QuantumSimError):
     """Exception raised for invalid parameters."""
-    def __init__(self, parameter, message="Invalid parameter provided"):
+    def __init__(self: InvalidParameterError, parameter, message: str ="Invalid parameter provided"):
         self.parameter = parameter
         self.message = message
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self: InvalidParameterError):
         return f"{self.message}: {self.parameter}"
 
 class SimulationError(QuantumSimError):
     """Exception raised for errors during simulation."""
-    def __init__(self, message="An error occurred during the simulation"):
+    def __init__(self: QuantumSimError, message: str="An error occurred during the simulation"):
         self.message = message
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self: QuantumSimError):
         return self.message
 
--- src/quantum_sim/validators/wave_validators.py
+++ src/quantum_sim/validators/wave_validators.py
@@ -1,19 +1,19 @@
 # src/quantum_sim/validators/wave_validators.py
 from ..errors.exceptions import InvalidParameterError
 
-def validate_positive(value, parameter_name="value"):
+def validate_positive(value: int | float, parameter_name: str = "value") -> bool:
     """Validate that the given value is positive."""
     if value <= 0:
         raise InvalidParameterError(parameter=parameter_name, message="Value must be positive")
     return True
 
-def validate_non_negative(value, parameter_name="value"):
+def validate_non_negative(value: int | float, parameter_name: str = "value") -> bool:
     """Validate that the given value is non-negative."""
     if value < 0:
         raise InvalidParameterError(parameter=parameter_name, message="Value must be non-negative")
     return True
 
-def validate_range(value, min_value, max_value, parameter_name="value"):
+def validate_range(value: int | float, min_value: int | float, max_value: int | float, parameter_name: str = "value") -> bool:
     """Validate that the given value is within the specified range [min_value, max_value]."""
     if not (min_value <= value <= max_value):
         raise InvalidParameterError(
