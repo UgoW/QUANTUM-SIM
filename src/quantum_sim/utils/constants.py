ed81302aa4faf752300a0622742bb111986311dc
Williampasset <william.passet@student.junia.com>
2026-01-19 11:06:08 +0100

    feat(physics-constants): init constants file in utils module with constants name and value and add unit test for it

---
+++ src/quantum_sim/utils/constants.py
@@ -0,0 +1,10 @@
+# src/quantum_sim/utils/constants.py
+
+import numpy as np
+
+# Physical constants
+PLANCK_CONSTANT = 6.62607015e-34  # in J·s
+PI = np.pi # Dimensionless
+REDUCED_PLANCK_CONSTANT = PLANCK_CONSTANT / (2 * PI)  # in J·s
+ELECTRON_MASS = 9.10938356e-31  # in kg
+SPEED_OF_LIGHT = 2.99792458e8  # in m/s
