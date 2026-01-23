a8166e19f4972516222fa928f58af20cc4b91987
William Passet <112688839+Williampasset@users.noreply.github.com>
2026-01-23 09:23:46 +0100

    feat(waves-params): add waves params (#22)
    * feat(params): start init params, setter, getter for PlaneWave and init unit test for it
    
    * feat(classes): change file name to respect best practices
    
    * feat(params): add unit test for plane wave which test almost every important case

---
+++ src/quantum_sim/waves/plane_wave.py
@@ -0,0 +1,48 @@
+import numpy as np
+from quantum_sim.waves import WaveFunction
+from quantum_sim.utils.constants import ELECTRON_MASS, REDUCED_PLANCK_CONSTANT, PI
+from quantum_sim.validators.wave_validators import validate_positive, validate_non_negative, validate_range
+
+class PlaneWave(WaveFunction):
+    """
+    Plane wave implementation: ψ(x,t) = A*exp(i(k(x - x0) - ωt + φ))
+    """
+    
+    def __init__(self, amplitude: complex, wavelength: float,
+                 position: float = 0.0, phase: float = 0.0, time: float = 0.0, masse: float = ELECTRON_MASS):
+        """
+        Initialize plane wave.
+        
+        Args:
+            amplitude: Wave amplitude
+            wavelength: Wavelength λ
+            time: Time parameter t
+            position: Initial spatial offset x0
+            phase: Initial phase φ
+            masse: Particle mass
+        """
+        self.amplitude = amplitude
+        self.wavelength = wavelength
+        self.position = position
+        self.phase = phase
+        self.masse = masse
+        super().__init__(np.array([position]), time)
+    
+    def validate_parameters(self) -> None:
+        """Validate parameters of the plane wave."""
+        validate_positive(self.wavelength, "wavelength")
+        validate_non_negative(self.masse, "masse")
+    
+    def wave_number(self) -> float:
+        """Calculate wave number k = 2π/λ."""
+        return 2 * PI / self.wavelength
+
+    def angular_frequency(self) -> float:
+        """Calculate angular frequency ω = ħk²/(2m)."""
+        k = self.wave_number()
+        return (REDUCED_PLANCK_CONSTANT * k**2) / (2 * self.masse)
+        
+    def evaluate(self, x: float | np.ndarray) -> np.ndarray:
+        """Evaluate plane wave."""
+        # TODO: Implement evaluation
+        pass
